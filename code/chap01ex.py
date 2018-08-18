569ca99ad9fd548213865a516a43c41295049f36
Dimitrios <dimitrios.theoharatos@gmail.com>
2018-08-17 17:17:44 -0700

    completed CH2 exercises

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,6 +13,36 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemPreg(dct_file='2002FemPreg.dct',
+                dat_file='2002FemPreg.dat.gz'):
+    """Reads the NSFG pregnancy data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    CleanFemPreg(df)
+    return df
+
+
+
+def ValidatePregnum(resp):
+	   """Validate pregnum in the respondent file.
+
+    resp: respondent DataFrame
+    """
+    # read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+
+    for index, pregnum in resp.pregnum.item():
+    	caseid = resp.caseid[index]
+    	incie
 
 def main(script):
     """Tests the functions in this module.
